638b9e85b8488045cce293a5802ee226018017f0
rinox3005 <peppe.rinox@gmail.com>
2024-05-13 17:33:17 +0200

    [UPD] countdown function and event listener done

---
+++ README.md
@@ -0,0 +1,11 @@
+repo: js-countdown
+
+Simulare un count down di 10 secondi che alla fine dice "Buon anno!"
+
+Potete stampare il messaggio in pagina con un semplice alert.
+
+Bonus 1
+Mostrate il conteggio alla rovescia in pagina;
+
+Bonus 2
+Aggiungete un bottone che interrompe il count down e mostra il messaggio;
--- js/main.js
+++ js/main.js
@@ -1,2 +1,45 @@
 'use strict';
 
+// prendo gli elementi che mi servono dalla pagina html
+const number = document.getElementById('number');
+const startCountdown = document.getElementById('startcountdown');
+const resetCountdown = document.getElementById('resetcountdown');
+
+// inizializzo le variabili
+const start = 10;
+let counter = start;
+let myCountdown;
+let countdownStarted = false;
+
+// mi metto in ascolto degli eventi sulla pagina
+
+startCountdown.addEventListener('click', function () {
+    if (countdownStarted == false) {
+        myCountdown = setInterval(countdown, 1000);
+    }
+    countdownStarted = true;
+})
+
+resetCountdown.addEventListener('click', function () {
+    resetCount();
+})
+
+
+// funzione countdown
+
+function countdown() {
+    if (counter > 1) {
+        counter--;
+        number.innerText = counter;
+    } else if (counter === 1) {
+        number.innerText = 'Buon Anno!';
+    }
+}
+
+// funzione reset
+
+function resetCount() {
+    number.innerText = counter = start;
+    countdownStarted = false;
+    clearInterval(myCountdown);
+}
